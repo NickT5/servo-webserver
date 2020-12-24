2d7a6169553dd963f5667fb143dbd55a4be9f1fc
Nick <nick-todts@hotmail.com>
2020-12-24 15:09:59 +0100

    api to control servo

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
+import json
+from time import sleep
 import RPi.GPIO as GPIO
 from flask import Flask, request
+
 app = Flask(__name__)
 
 
 def angle_to_duty_cycle(angle: float) -> float:
     """
     convert angle to duty cycle
-    :param angle:
-    :return:
     """
-    return (1.0/18.0 * angle) + 2
+    try:
+        dc = (1.0/18.0 * angle) + 2
+    except TypeError:
+        dc = None
+    return dc
 
 
 @app.route('/')
@@ -17,27 +22,53 @@ def hello_world():
     return 'Hello, World!'
 
 
-# http://localhost:5000/servo?angle=5
 @app.route("/servo")
-def set_angle():
-    angle = request.args.get('angle')
-    try:
-        angle = float(angle)
-    except:
-        pass
+def set_servo_angle():
+    # e.g. http://<rpi-ip>:<port>/servo?angle=90
+    angle = request.args.get('angle', type=float)
+    duty_cycle = angle_to_duty_cycle(angle)
 
-    return angle
+    if duty_cycle is None:
+        response: dict = {
+	    "angle": angle,
+	    "duty_cycle": duty_cycle,
+	    "status": 400
+        }
+    else:
+        GPIO.output(SERVO_PIN, True)
+        pwm.ChangeDutyCycle(duty_cycle)
+
+        sleep(1)  # give time for servo to turn
+
+        # turn off pwm to stop buzzing
+        # buzzing might go away if we
+        # use a diff power source for the motor
+        GPIO.output(SERVO_PIN, False)
+        pwm.ChangeDutyCycle(0)
+
+        response: dict = {
+	    "angle": angle,
+	    "duty_cycle": duty_cycle,
+	    "status": 200
+        }
+
+    return json.dumps(response)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    # setup gpio pin for servo
     GPIO.setmode(GPIO.BOARD)
     SERVO_PIN = 3
     GPIO.setup(SERVO_PIN, GPIO.OUT)
-
+    # setup pwm for servo
     FREQUENCY = 50
     pwm = GPIO.PWM(SERVO_PIN, FREQUENCY)
     pwm.start(0)
+    # setup server
+    PORT = 5001
+    print(f"Server is listening on port {PORT}")
+    app.run(debug=True, host="0.0.0.0", port=PORT)
 
-    app.run(debug=True)
-
-    print("hello")
+    # tear down
+    pwm.stop()
+    GPIO.cleanup()
